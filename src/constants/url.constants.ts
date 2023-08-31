2b167f9624f9c35cc1e85ce29f67c068980c029e
Armaan Jain <armaanjain@Armaans-MacBook-Pro-16.local>
2023-09-01 00:00:00 +0530

    Committing file ./src/constants/url.constants.ts on date 2023-09-01

---
+++ src/constants/url.constants.ts
@@ -0,0 +1,7 @@
+export const URL = {
+    PORT: process.env.PORT,
+    BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
+    MODE: process.env.REACT_APP_MODE,
+    OLD_GA_URL: process.env.REACT_APP_OLD_GA_URL,
+    REACT_APP_GA_ID: process.env.REACT_APP_GA_ID,
+}
